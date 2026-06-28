6672fac5ccaa95bb7dbb110a8942a5221a3c5065
unknown <thornsambatt@umekratie.edu.kh>
2026-06-28 10:47:34 +0700

    update: icons, 'contact_us': phone number's spacing.

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -10,11 +10,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     # 6 explicit vertical buttons mapping to the image layout
     keyboard = [
-        [InlineKeyboardButton("អំពីសាកលវិទ្យាល័យ", callback_data="about_uni")],
-        [InlineKeyboardButton("🎓🧑‍🎓 ព័ត៌មានសិក្សា 🧑‍🎓🎓", callback_data="academic_info")],
-        [InlineKeyboardButton("✍️🎓 ការចុះឈ្មោះចូលរៀន ✍️🎓", callback_data="registration")],
-        [InlineKeyboardButton("✍️💧 បង់ថ្លៃសិក្សា ✍️🚰", callback_data="tuition_payment")],
-        [InlineKeyboardButton("ទំនាក់ទំនង 📞", callback_data="contact_us")],
+        [InlineKeyboardButton("💡អំពីសាកលវិទ្យាល័យ", callback_data="about_uni")],
+        [InlineKeyboardButton("🎓🧑‍🎓 ព័ត៌មានសិក្សា 🎓🧑‍🎓", callback_data="academic_info")],
+        [InlineKeyboardButton("🎓✍️ ការចុះឈ្មោះចូលរៀន ✍️🎓", callback_data="registration")],
+        [InlineKeyboardButton("✍️💧 បង់ថ្លៃសិក្សា 💧✍️", callback_data="tuition_payment")],
+        [InlineKeyboardButton("📞 ទំនាក់ទំនង", callback_data="contact_us")],
         [InlineKeyboardButton("📍 ទីតាំងសាកលវិទ្យាល័យ", callback_data="location")]
     ]
     
